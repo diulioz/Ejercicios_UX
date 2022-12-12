61d960b7b10b2e7715600e8f9538cf41c9ba0114
diulioz <diulioz.u@gmail.com>
2022-12-11 21:08:18 -0500

    punto3

--- js/punto3.js
+++ js/punto3.js
@@ -1,55 +1,92 @@
 function suma(evt){
     evt.preventDefault();
-    let a,b,c,d;
-    r1=0;
-    r2=0;
-    r3=0;
-    let cad= "";
-    let cad_2="";
-    let cad_3="";
-    let cad_4="";
-    let cad_5="";
-    let cadf="";
-    a=parseInt(document.getElementById("txtn1").value);
-    b=parseInt(document.getElementById("txtn2").value);
-    c=parseInt(document.getElementById("txtn3").value);
-    d=parseInt(document.getElementById("txtn4").value);
+    let h,m,s,su,aux, auxm, auxh, auxs;
+    r_horas=0;
+    r_minutos=0;
+    r_seg=0;
+    let cadH= "";
+    let cadM= "";
+    let cadS= "";
+    let cadf= "";
 
+    h=parseInt(document.getElementById("txtn1").value);
+    m=parseInt(document.getElementById("txtn2").value);
+    s=parseInt(document.getElementById("txtn3").value);
+    su=parseInt(document.getElementById("txtn4").value);
+    cadH= " "+ h + " h";
+    cadM= " "+ m + " m";
+    cadS= " "+ s + " s";
 
-    d=d%60;
-    if(a>50000){
-            r3=a
-            r2=a%60;
-            a=a-r2;
-            r1=a/50000
-            cad=r1 +" Billetes de $50000 \n";
-        if (r2>=20000){
-            a=r2;
-            r2=r2%20000;
-            a=a-r2;
-            r1=a/20000
-            cad_2=" "+ r1 +" Billetes de $20000\n"
-        }
-        if(r2>=10000){
-            a=r2;
-            r2=r2%10000;
-            a=a-r2;
-            r1=a/10000
-            cad_3=" "+ r1 +" Billetes de $10000\n"
-        }
-        if(r2>=5000){
-            a=r2;
-            r2=r2%5000;
-            a=a-r2;
-            r1=a/5000
-            cad_4=" "+ r1 +" Billetes de $5000\n"
-        }
-        if(r2<5000){
-            cad_5="Sobran $" + r2;
+    if(su>=3600){
+        auxh=su%3600;
+        aux=auxh;
+        r_horas=su-aux;
+        r_horas=r_horas/3600;
+        h=h+r_horas;
+        cadH= " "+ h + " h";
+    }    
+
+    if(su>59 & su<3600){
+        aux=su%60;
+        r_minutos=su-aux;
+        r_minutos=r_minutos/60;
+        m=m+r_minutos;
+        if (m>59){
+            h=h+1;
+            cadH= " "+ h + " h";
+            auxm=m%60;
+            m=auxm;
+            cadM= " "+ auxm + " m"
+        }else{
+            cadM= " "+ m + " m";
         }
     }
 
-    cadf=cad+cad_2+cad_3+cad_4+cad_5;
+    if(aux>59 & aux<3600){
+        auxm=aux%60;
+        r_minutos=aux-auxm;
+        aux=auxm;
+        r_minutos=r_minutos/60;
+        m=m+r_minutos;
+        if (m>59){
+            h=h+1;
+            cadH= " "+ h + " h";
+            auxm=m%60;
+            m=auxm;
+            cadM= " "+ auxm + " m"
+        }else{
+            cadM= " "+ m + " m";
+        }
+    }
 
+    if (su<60){
+        if((s+su)<60){
+            s=s+su;
+            cadS= " "+ s + " s";
+        }else{
+            auxs = s+su;
+            auxs=auxs%60;
+            s=auxs;
+            m=m+1;
+            cadM= " "+ m + " m"
+            cadS= " "+ s + " s";
+        }
+    }
+
+    if (aux<60){
+        if((aux+s)<60){
+            s=aux+s;
+            cadS= " "+ s + " s";
+        }else{
+            auxs = s+aux;
+            auxs=auxs%60;
+            s=auxs;
+            m=m+1;
+            cadM= " "+ m + " m"
+            cadS= " "+ s + " s";
+        }
+    }
+
+    cadf=cadH+cadM+cadS;
     alert(cadf);
 }
