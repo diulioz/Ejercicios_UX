16478aaf8a892e7e329e0d63ef99af8cbfe65830
diulioz <diulioz.u@gmail.com>
2022-12-11 21:37:32 -0500

    Final

--- js/punto2.js
+++ js/punto2.js
@@ -1,23 +1,5 @@
-function calcularp2(){
-    evt.preventDefault();
-    let h=parseInt(document.getElementById("txtn1").value);
-    let m=parseInt(document.getElementById("txtn2").value);
-    let s=parseInt(document.getElementById("txtn3").value); 
-
-    let tiempoSeg = h*3600 + m*60 + s;
-    
-    let arcRad = ((2*Math.PI)/86400) * tiempoSeg;
-    let grados = arcRad*(180/Math.PI);
-    let gradosDecimales = grados - Math.floor(grados);
-    let minutos = gradosDecimales*60;
-    let minutosDecimales = minutos - Math.floor(minutos);
-    let segundos = minutosDecimales*60;
-    
-    alert("El resultado es Grados : " + Math.floor(grados) + " Minutos : " + Math.floor(minutos) + " Segundos : " + segundos)
-
-}
-
 function calcular(){
+    evt.preventDefault();
 
     let h, m, s,horas, grados, grados2, minutos, minutos2, segundos;
     h=parseInt(document.getElementById("txtn1").value);
